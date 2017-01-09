119d8a166b892f14e002d2933d9f04146ed82c7a
Vince Roti <vince.roti@gmail.com>
2017-01-08 22:13:11 -0600

    remove comments

--- src/App.js
+++ src/App.js
@@ -5,32 +5,7 @@ import './App.css';
 class Nav extends Component {
   render() {
     return (
-      <h3> a nav bar will here! </h3>
-//         <Navbar inverse collapseOnSelect>
-//           <Navbar.Header>
-//             <Navbar.Brand>
-//               <a href="#">React-Bootstrap</a>
-//             </Navbar.Brand>
-//             <Navbar.Toggle />
-//           </Navbar.Header>
-//           <Navbar.Collapse>
-//             <Nav>
-//               <NavItem eventKey={1} href="#">Link</NavItem>
-//               <NavItem eventKey={2} href="#">Link</NavItem>
-//               <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-//                 <MenuItem eventKey={3.1}>Action</MenuItem>
-//                 <MenuItem eventKey={3.2}>Another action</MenuItem>
-//                 <MenuItem eventKey={3.3}>Something else here</MenuItem>
-//                 <MenuItem divider />
-//                 <MenuItem eventKey={3.3}>Separated link</MenuItem>
-//               </NavDropdown>
-//             </Nav>
-//             <Nav pullRight>
-//               <NavItem eventKey={1} href="#">Link Right</NavItem>
-//               <NavItem eventKey={2} href="#">Link Right</NavItem>
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Navbar>
+      <h3> Cool React NavBar </h3>
       )
   }
 }
