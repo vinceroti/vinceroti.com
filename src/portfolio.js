d38ce4b4293bbe4b7be2ed835cec925e88cdedd6
Vince Roti <vince.roti@gmail.com>
2017-01-23 20:43:32 -0600

    include reactCSS

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,68 +1,70 @@
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import reactCSS, { hover } from 'reactcss';
 import React, { Component } from 'react';
 import { Glyphicon } from 'react-bootstrap';
 
 
-const styles = {
-  portfolio: {
-    backgroundColor: 'gray',
-    color: 'white',
-    margin: 'auto',
-    textAlign: 'center',
-    paddingTop: '50px'
-  },
+const styles = reactCSS({
+  default: {
+    portfolio: {
+      backgroundColor: 'gray',
+      color: 'white',
+      margin: 'auto',
+      textAlign: 'center',
+      paddingTop: '50px'
+    },
 
-  line: {
-    padding: '0',
-    border: 'none',
-    borderTop: 'solid 5px',
-    textAlign: 'center',
-    width: '100px',
-    maxWidth: '20%',
-    margin: 'auto',
-    backgroundColor: '#18BC9C',
-    color: 'white',
-    display: 'inline-block',
-    borderRadius: '250px',
+    line: {
+      padding: '0',
+      border: 'none',
+      borderTop: 'solid 5px',
+      textAlign: 'center',
+      width: '100px',
+      maxWidth: '20%',
+      margin: 'auto',
+      backgroundColor: '#18BC9C',
+      color: 'white',
+      display: 'inline-block',
+      borderRadius: '250px',
 
-  },
+    },
 
-  star: {
-    display: 'inline-block',
-    position: 'relative',
-    top: '11px',
-    fontSize: '2em',
-    padding: '0 0.25em',
-    borderColor: 'white',
-  },
+    star: {
+      display: 'inline-block',
+      position: 'relative',
+      top: '11px',
+      fontSize: '2em',
+      padding: '0 0.25em',
+      borderColor: 'white',
+    },
 
-  title: {
-    fontWeight: 'bold',
-    fontSize: '40px',
-    letterSpacing: '2.5px'
-  },
+    title: {
+      fontWeight: 'bold',
+      fontSize: '40px',
+      letterSpacing: '2.5px'
+    },
 
-  ul: {
-    paddingLeft: '0',
-    listStyleType: 'none'
-  },
-
-  image: {
-    display: 'inline-block',
-    margin: 'auto',
-    marginTop: '20px',
-    width: '300px',
-    maxWidth: '90%',
+    ul: {
+      paddingLeft: '0',
+      listStyleType: 'none'
+    },
+    image: {
+      display: 'inline-block',
+      margin: 'auto',
+      marginTop: '20px',
+      width: '300px',
+      maxWidth: '90%',
+    },
+    hover: {
+      image: {
+        transform: 'scale(1.1)',
+      },
+    },
   }
-}
+})
 
 
 
 class Portfolio extends Component {
-
-  handleMouseOver(){
-    return console.log("test")
-  }
 
   render() {
 
@@ -74,8 +76,10 @@ class Portfolio extends Component {
         <hr style={styles.line} />
         <div>
           <ul style={styles.ul}>
-            <li style={styles.image}><img style={styles.image} src="wiki.png" alt="Wikipedia Logo" /></li>
-            <li style={styles.image}><img onMouseOver={this.handleMouseOver} style={styles.image} src="google.png" alt="Google Logo" /></li>
+
+              <li style={styles.image}><img style={styles.image} src="wiki.png" alt="Wikipedia Logo" /></li>
+
+              <li style={styles.image}><img style={styles.image}  src="google.png" alt="Google Logo" /></li>
             </ul>
         </div>
       </div>
@@ -83,4 +87,7 @@ class Portfolio extends Component {
   }
 }
 
+
+
+Portfolio = hover(Portfolio)
 export { Portfolio };
