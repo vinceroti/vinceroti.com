2ff96a2533c9a4bd6feffbb7a4c76d83cfa6fa3d
Vince Roti <vince.roti@gmail.com>
2017-01-25 13:14:13 -0600

    add smooth transition on load

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import Header from './components/header';
 import NavBar from './components/navbar';
 import Portfolio from './components/portfolio';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import './styles/index.css';
 
 ReactDOM.render(
   <div>
     <NavBar />
-    <Header />
+    <ReactCSSTransitionGroup
+      transitionName="example"
+      transitionAppear={true}
+      transitionAppearTimeout={700}
+      transitionEnter={false}
+      transitionLeave={false}>
+      <Header />
+    </ReactCSSTransitionGroup>
     <Portfolio/>
   </div>,
   document.getElementById('root')
