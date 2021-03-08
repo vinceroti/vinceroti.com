311838f6236635387d958fb6be3afa9d2f58a942
Vince Roti <vince.roti@gmail.com>
2021-03-07 16:22:38 -0800

    add svgs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,14 +5,33 @@
     </div>
     <div class="copy">
       <h1>Hi,<br />I'm Vince, <br />Web Developer.</h1>
+      <a
+        href="https://github.com/vinceroti/"
+        title="Vince Roti's Github Profile"
+      >
+        <github />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/vinceroti/"
+        title="Vince Roti's Linkedin Profile"
+      >
+        <linkedin />
+      </a>
     </div>
   </section>
 </template>
 
 <script>
 import image from "../assets/vince.jpg";
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.svg";
+
 export default {
   name: "home",
+  components: {
+    github,
+    linkedin,
+  },
   data() {
     return {
       image,
@@ -22,6 +41,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h1 {
+  margin-bottom: spacing(base);
+}
+svg {
+  width: rem(75);
+  height: rem(75);
+}
+a {
+  .github {
+    border: 2px solid color(black);
+    margin-right: spacing(half);
+    .social-svg-icon {
+      fill: color(black);
+    }
+  }
+  &:hover .linkedin .social-svg-icon {
+    fill: color(black);
+  }
+  &:hover .github {
+    background-color: color(black);
+    .social-svg-icon {
+      fill: color(white);
+    }
+  }
+}
 .home {
   display: flex;
   flex-wrap: wrap;
